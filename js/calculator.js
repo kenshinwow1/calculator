03cc83d4cd63fd50eec2229268dd4aea2e84b0ca
Ualink <kenshinwow1@gmail.com>
2023-06-10 00:12:45 +0300

    all ok

---
+++ js/calculator.js
@@ -0,0 +1,34 @@
+const OPERATION = {
+    sum: '+',
+    substract: '-',
+    multiply: '*',
+    division: '/'
+};
+
+function calculate(a, b, operation){
+
+    let result = null;
+
+    switch (operation) {
+        case OPERATION.sum:
+            result = sum (a, b);
+            break;
+
+        case OPERATION.substract:
+            result = substract (a, b);
+            break;
+
+        case OPERATION.multiply:
+            result = multiply (a, b);
+            break;
+
+        case OPERATION.division:
+            result = division (a, b);    
+            break;
+    
+        default:
+            break;
+    }
+
+    return result;
+}
